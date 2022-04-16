77dd079e62937c71a825c8db087682b464070e01
Andrey Frimuchkov <afrimuchkov@yandex.ru>
2022-04-16 14:58:10 +0300

    Add Client type available to import

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,8 @@
-import Client from './client';
+import MailgunClient from './client';
 import { InputFormData } from './interfaces/IFormData';
 import Options from './interfaces/Options';
+
+export type Client = MailgunClient;
 
 export default class Mailgun {
   static get default(): typeof Mailgun { return this; }
@@ -10,7 +12,7 @@ export default class Mailgun {
     this.formData = FormData;
   }
 
-  client(options: Options) : Client {
-    return new Client(options, this.formData);
+  client(options: Options) : MailgunClient {
+    return new MailgunClient(options, this.formData);
   }
 }
